ddfd3b746e1879004d9cc3cb425e0f4b28c50028
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 08:43:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
     array.unshift(element);
     return array;
 }
+
+function addElementToEndOfArray(inputArray, element) {
+    var outputArray = [...inputArray, element];
+    return outputArray;
+}
