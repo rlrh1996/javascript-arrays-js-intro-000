575941204e61d823b3a87227011e36e68191a598
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 08:43:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(inputArray, element) {
-    var outputArray = inputArray;
-    outputArray.unshift(element);
+    var outputArray = [element, ...inputArray];
     return outputArray;
 }
 
